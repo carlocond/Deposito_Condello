f8e019fc9f5a32f82e5041d1e10efe519c1822bd
carlocond <carlocondello05@gmail.com>
2025-05-20 16:12:49 +0200

    Add files via upload
    +Aggiunta dei file presenti nella cartella del corso c#

---
+++ Lezione7_Esercizio1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Punto
+{
+    public int x;
+    public int y;
+
+    //Sovrascrive Equals per confrontare le coordinate assegnate
+    public override bool Equals(object? obj)
+    {
+        if (obj is Punto other)
+        {
+            return this.x == other.x && this.y == other.y;
+        }
+        return false;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Punto a = new Punto { x = 1, y = 2 };
+            Punto b = new Punto { x = 2, y = 3 };
+
+            Console.WriteLine(a.Equals(b));
+            //Output false (diversi oggetti differenti)
+
+            Punto c = new Punto { x = 1, y = 2 };
+            Console.WriteLine(a.Equals(c));
+            //Output true (diversi oggetti uguali)
+        }
+    }
+}
