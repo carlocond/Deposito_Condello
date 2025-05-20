201efce94b31df6750cc81e65ad52cef31218ccf
carlocond <carlocondello05@gmail.com>
2025-05-20 17:25:43 +0200

    Add files via upload
    +Aggiunta dei file presenti nella cartella del corso c#

---
+++ Lezione7_Esercizio6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Dichiarazione della grandezza dell'array
+        int numVoti = 5;
+
+        //Dichiarazione dell'array
+        int[] voti = new int[numVoti];
+
+        //Inizio del ciclo per inserire i numeri
+        for (int i = 0; i < numVoti; i++)
+        {
+            Console.WriteLine("Inserisci i voti");
+            voti[i] = int.Parse(Console.ReadLine());
+        }
+
+        //Dichiarazione di variabili di riferimento
+
+        int votoMax = voti[0];
+        int votoMin = voti[0];
+        int somma = 0;
+
+
+        //Creazione del ciclo di controllo di ogni voto
+
+        for (int i = 0; i < voti.Length; i++)
+        {
+            if (voti[i] > votoMax)
+            {
+                votoMax = voti[i];
+            }
+            else if (voti[i] < votoMin)
+            {
+                votoMin = voti[i];
+            }
+            somma += voti[i];
+            
+        }
+        double media = somma / numVoti;
+        //Stampa dei voti
+
+        Console.WriteLine($"Il voto più alto è {votoMax}");
+        Console.WriteLine($"Il voto più basso è {votoMin}");
+        Console.WriteLine($"La media di tutti i voti è {media}");
+    }
+}
---
+++ Lezione7_Esercizio7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> spesa = new List<string>();
+
+        Console.WriteLine("Inserisci 5 prodotti nella lista della spesa");
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Aggiungi un prodotto");
+            string prodotto = Console.ReadLine();
+            spesa.Add(prodotto);
+        }
+        Console.WriteLine("I prodotti aggiunti alla spesa sono: ");
+        foreach (string prodotto in spesa)
+        {
+            Console.WriteLine($"- {prodotto}");
+        }
+    }
+}
