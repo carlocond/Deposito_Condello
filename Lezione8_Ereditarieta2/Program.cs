47b6cffcb7d5ee6c35500fca6faa0c5e39517ba9
carlocond <carlocondello05@gmail.com>
2025-05-21 16:28:09 +0200

    Add files via upload
    +Aggiornamento del file Lezione8_Ereditarieta2

--- Lezione8_Ereditarieta2/Program.cs
+++ Lezione8_Ereditarieta2/Program.cs
@@ -1,25 +1,29 @@
 ﻿using System;
 
+//Creazione della classe principale
 public class Veicolo
 {
     public string modelloV;
     public string marcaV;
 
-
+//Costruttore della classe
     public Veicolo(string modello, string marca)
     {
         modelloV = modello;
         marcaV = marca;
     }
 
-       public virtual string toString()
+//Creazione del metodo di stampa delle informazioni
+    public virtual string toString()
     {
         return $"La marca è: {marcaV}, il modello è {modelloV}";
     }
 }
 
+//Creazione della prima classe ereditaria
 public class Auto : Veicolo
 {
+    //Aggiunta di un'altra variabile non ereditata
     public int numeroPorte;
 
     public Auto(string modello, string marca, int numPorte) : base(modello, marca)
@@ -27,13 +31,16 @@ public class Auto : Veicolo
         numeroPorte = numPorte;
     }
 
+    //Override del metodo di stampa in base al veicolo specifico
     public override string toString()
     {
         return $"Il modello dell'auto {marcaV} è {modelloV}, ha {numeroPorte} porte";
     }
 }
+//Creazione della seconda classe ereditaria
 public class Moto : Veicolo
 {
+    //Aggiunta di un'altra variabile non ereditata
     public string tipoManubrio;
 
     public Moto(string modello, string marca, string manubrio) : base(modello, marca)
@@ -41,12 +48,14 @@ public class Moto : Veicolo
         tipoManubrio = manubrio;
     }
 
+//Override del metodo di stampa in base al veicolo specifico
     public override string toString()
     {
         return $"Il modello della moto {marcaV} è {modelloV}, ha un manubrio stile {tipoManubrio}";
     }
 }
 
+//Creazione della classe del programma
 public class Program
 {
     public static void Main(string[] args)
@@ -55,6 +64,7 @@ public class Program
 
         bool continua = true;
 
+//Creazione del ciclo del menù di scelta
         while (continua)
         {
             Console.WriteLine("Scegli una tra le 3 opzioni");
@@ -64,41 +74,42 @@ public class Program
             switch (scelta)
             {
                 case 1:
+//Inizializzazione variabili della moto
+                    Console.WriteLine("Inserisci la marca");
+                    string marcaMoto = Console.ReadLine();
 
-                Console.WriteLine("Inserisci la marca");
-                string marcaMoto = Console.ReadLine();
+                    Console.WriteLine("Inserisci il modello");
+                    string modelloMoto = Console.ReadLine();
 
-                Console.WriteLine("Inserisci il modello");
-                string modelloMoto = Console.ReadLine();
+                    Console.WriteLine("Inserisci lo stile del manubrio");
+                    string stileManubrio = Console.ReadLine();
 
-                Console.WriteLine("Inserisci lo stile del manubrio");
-                string stileManubrio = Console.ReadLine();
+//Creazione oggetto e aggiunta alla lista
+                    Moto nuovaMoto = new Moto(marcaMoto, modelloMoto, stileManubrio);
+                    garage.Add(nuovaMoto);
+                    break;
 
-                Moto nuovaMoto = new Moto(marcaMoto, modelloMoto, stileManubrio);
-                garage.Add(nuovaMoto);
-                break;
+                case 2:
+//Inizializzazione delle variabili dell'auto
+                    Console.WriteLine("Inserisci la marca");
+                    string marcaAuto = Console.ReadLine();
 
-            case 2:
+                    Console.WriteLine("Inserisci il modello");
+                    string modelloAuto = Console.ReadLine();
 
-                Console.WriteLine("Inserisci la marca");
-                string marcaAuto = Console.ReadLine();
+                    Console.WriteLine("Inserisci il numero delle porte");
+                    int numPorte = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Inserisci il modello");
-                string modelloAuto = Console.ReadLine();
+//Creazione oggetto e aggiunta alla lista
+                    Auto nuovaAuto = new Auto(marcaAuto, modelloAuto, numPorte);
+                    garage.Add(nuovaAuto);
 
-                Console.WriteLine("Inserisci il numero delle porte");
-                int numPorte = int.Parse(Console.ReadLine());
-
-                Auto nuovaAuto = new Auto(marcaAuto, modelloAuto, numPorte);
-
-                garage.Add(nuovaAuto);
-
-                break;
+                    break;
 
 
-            case 3:
-
-            Console.WriteLine("Ecco i veicoli presenti in garage");
+                case 3:
+//Visualizzazione di ogni veicolo presente nel garage
+                    Console.WriteLine("Ecco i veicoli presenti in garage");
 
                     foreach (Veicolo v in garage)
                     {
@@ -106,21 +117,21 @@ public class Program
                     }
                     break;
 
-            case 4:
+                case 4:
+//Chiusura del programma
+                    Console.WriteLine("Arrivederci");
+                    continua = false;
+                    break;
 
-                Console.WriteLine("Arrivederci");
-                continua = false;
-                break;
-
-            default:
-            
-                Console.WriteLine("ERRORE");
-                continua = false;
-                break;
+                default:
+//Chiusura forzata del programma
+                    Console.WriteLine("ERRORE");
+                    continua = false;
+                    break;
+            }
         }
+
     }
-            
-}
 
 }
 
