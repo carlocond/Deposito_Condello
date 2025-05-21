3b4eea343ce284c9b7e1b29ea5da07f904d6ec2c
carlocond <carlocondello05@gmail.com>
2025-05-21 12:58:05 +0200

    Add files via upload
    +Aggiunta dei file presenti nella cartella del corso c#

---
+++ Lezione8_Ereditarieta/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//Creazione classe base
+public class Animale
+{
+    public void FaiVerso()
+    {
+        Console.WriteLine("L'animale fa un verso.");
+    }
+}
+
+//Creazione della classe derivata
+public class Cane : Animale
+{
+    public void Scondizola()
+    {
+        Console.WriteLine("Il cane scondinzola.");
+    }
+}
+
+//Programma principale
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Cane cane1 = new Cane(); //Creazione di un oggetto della classe derivata
+        cane1.FaiVerso(); //Metodo ereditato dalla classe base
+        cane1.Scondizola(); //Metodo definito nella classe derivata
+    }
+}
