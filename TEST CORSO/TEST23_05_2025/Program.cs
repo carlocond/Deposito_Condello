d2cbb17e7a734951fa7121a17d63bc2d008df69c
carlocond <carlocondello05@gmail.com>
2025-05-23 15:47:17 +0200

    TEST CORSO C#

---
+++ TEST CORSO/TEST23_05_2025/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Runtime.Versioning;
+
+public class Veicolo
+{
+    private string marca;
+    public string Marca
+    {
+        get { return marca; }
+        set { marca = value; }
+    }
+    private string modello;
+    public string Modello
+    {
+        get { return modello; }
+        set { modello = value; }
+    }
+    private int annoImmatricolazione;
+    public int AnnoImmatricolazione
+    {
+        get { return annoImmatricolazione; }
+        set { annoImmatricolazione = value; }
+    }
+
+    public virtual void StampaInfo()
+    {
+        Console.WriteLine("Ciao Mirko");
+    }
+}
+
+public class AutoAziendale : Veicolo
+{
+    private string targa;
+    public string Targa
+    {
+        get { return targa; }
+        set { targa = value; }
+    }
+    private bool usoPrivato;
+    public bool UsoPrivato
+    {
+        get { return usoPrivato; }
+        set { usoPrivato = value; }
+    }
+
+    public override void StampaInfo()
+    {
+        Console.WriteLine($"Il veicolo in utilizzo è una {Marca}, modello {Modello}, immatricolata nel {AnnoImmatricolazione}. La sua targa è {Targa}, Utilizzo privato : {UsoPrivato}");
+    }
+}
+
+public class FurgoneAziendale : Veicolo
+{
+    private int capacitaCarico;
+    public int CapacitaCarico
+    {
+        get { return capacitaCarico; }
+        set { capacitaCarico = value; }
+    }
+
+    public override void StampaInfo()
+    {
+        Console.WriteLine($"Il veicolo utilizzato è una {Marca}, modello {Modello}, immatricolata nel {AnnoImmatricolazione}. La capacità carico è: {capacitaCarico}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Veicolo> garageAziendale = new List<Veicolo>();
+
+        bool continua = true;
+
+        while (continua)
+        {
+            Console.WriteLine("\n--- MENU GARAGE ---");
+            Console.WriteLine("Scegli una delle opzioni \n[1]. Inserisci Auto \n[2].Inserisci Moto \n[3] Mostra tutti i veicoli \n[4] Esci");
+            string scelta = Console.ReadLine();
+
+            switch (scelta)
+            {
+                case "1":
+                    AutoAziendale auto = new AutoAziendale();
+                    Console.Write("Inserisci la marca: ");
+                    auto.Marca = Console.ReadLine();
+                    Console.Write("Inserisci il modello: ");
+                    auto.Modello = Console.ReadLine();
+                    Console.WriteLine("Inserisci l'anno di immatricolazione");
+                    auto.AnnoImmatricolazione = int.Parse(Console.ReadLine());
+
+                    Console.Write("Inserisci la targa: ");
+                    auto.Targa = Console.ReadLine();
+                    Console.WriteLine("E' di utilizzo privato? s o n");
+                    string risposta = Console.ReadLine();
+
+                    if (risposta == "s")
+                    {
+                        auto.UsoPrivato = true;
+                    }
+                    else if (risposta == "n")
+                    {
+                        auto.UsoPrivato = false;
+                    }
+                    else
+                    {
+                        Console.Write("Errore.");
+                    }
+
+                    garageAziendale.Add(auto);
+                    break;
+
+                case "2":
+                    FurgoneAziendale furgone = new FurgoneAziendale();
+
+                    Console.Write("Inserisci la marca: ");
+                    furgone.Marca = Console.ReadLine();
+                    Console.Write("Inserisci il modello: ");
+                    furgone.Modello = Console.ReadLine();
+                    Console.WriteLine("Inserisci l'anno di immatricolazione");
+                    furgone.AnnoImmatricolazione = int.Parse(Console.ReadLine());
+
+                    Console.Write("Inserisci il valore del carico: ");
+                    furgone.CapacitaCarico = int.Parse(Console.ReadLine());
+
+                    garageAziendale.Add(furgone);
+                    break;
+
+                case "3":
+                    Console.WriteLine("\n--- VEICOLI IN GARAGE ---");
+                    foreach (Veicolo v in garageAziendale)
+                    {
+                        Console.WriteLine();
+                        v.StampaInfo(); // Polimorfismo
+                        Console.WriteLine();
+                    }
+                    break;
+
+                case "4":
+                    Console.WriteLine("Ciao campione");
+                    continua = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Scelta non valida.");
+                    break;
+            }
+        }
+    }
+}
---
+++ TEST CORSO/TEST23_05_2025_ESERCIZIO2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+public class ContoCorrente
+{
+    private decimal saldo;
+
+    public decimal Saldo
+    {
+        get { return saldo; }
+        set
+        {
+            if (value >= 0)
+            {
+                saldo = value;
+            }
+        }
+    }
+    private int numeroOperazioni;
+    public int NumeroOperazioni
+    {
+        get { return numeroOperazioni; }
+        set
+        {
+            if (value >= 0)
+            {
+                numeroOperazioni = value;
+            }
+        }
+    }
+    public void Versa(decimal importo)
+    {
+        if (importo == 0)
+        {
+            Console.WriteLine("Numero inserito non valido");
+        }
+        else if (importo >= 0)
+        {
+            saldo += importo;
+            Console.WriteLine($"{importo}€ caricati");
+        }
+        else
+        {
+            Console.WriteLine("Non valido");
+        }
+    }
+
+    public void Preleva(decimal importo)
+    {
+        if (importo <= 0)
+        {
+            Console.WriteLine("Numero inserito non valido");
+        }
+        else if (importo <= Saldo)
+        {
+            saldo -= importo;
+            Console.WriteLine($"{importo}€ sono stati prelevati dal tuo conto");
+        }
+        else
+        {
+            Console.WriteLine("Non puoi prelevare più soldi di quelli disponibili");
+        }
+    }
+
+    public string toString()
+    {
+        return $"Il saldo è {saldo}, le operazioni effettuate sono {numeroOperazioni}";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ContoCorrente conto = new ContoCorrente();
+
+        bool continua = true;
+
+        while (continua)
+        {
+            Console.WriteLine("Vuoi prelevare o versare o stampare | p - v - s - uscire dal programma u?");
+            string scelta = Console.ReadLine();
+
+            switch (scelta)
+            {
+                case "p":
+                    Console.WriteLine("Quanto vuoi prelevare");
+                    int prelevaValore = int.Parse(Console.ReadLine());
+                    conto.Preleva(prelevaValore);
+                    conto.NumeroOperazioni++;
+                    break;
+                case "v":
+                    Console.WriteLine("Quanto vuoi versare?");
+                    int versaValore = int.Parse(Console.ReadLine());
+                    conto.Versa(versaValore);
+                    conto.NumeroOperazioni++;
+                    break;
+                case "s":
+                    Console.WriteLine(conto.toString());
+                    break;
+                case "u":
+                    Console.WriteLine("Arrivederci");
+                    continua = false;
+                    break;
+                default:
+                    Console.WriteLine("Hai sbagliato campione");
+                    conto.toString();
+                    break;
+            }
+        }
+    }
+}
